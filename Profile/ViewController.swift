0ecbf168b2bbd03f6212e00e985a86c6d94eac58
Yaroslav <neptun2001yarik@gmail.com>
2023-08-01 14:13:24 +0300

    add picture

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -19,17 +19,37 @@ class ViewController: UIViewController {
             label.textColor = .black
             var paragraphStyle = NSMutableParagraphStyle()
             paragraphStyle.lineHeightMultiple = 1.05
-            label.backgroundColor = .green
             label.textAlignment = .center
             return label
         }()
         
-        view.addSubview(headText)
         
+        
+        let image: UIView = {
+            let view = UIView()
+            view.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+            let image0 = UIImage(named: "Image.png")?.cgImage
+            let layer0 = CALayer()
+            layer0.contents = image0
+            layer0.bounds = view.bounds
+            layer0.position = view.center
+            view.layer.addSublayer(layer0)
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+        let name: UILabel = {
+            let label = UILabel()
+            label.text = "Савин Ярослав"
+            return label
+        }()
+        view.addSubview(headText)
+        view.addSubview(image)
         
         headText.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         headText.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         headText.topAnchor.constraint(equalTo: view.topAnchor, constant: 62).isActive = true
+        image.topAnchor.constraint(equalTo: headText.bottomAnchor, constant: 50).isActive = true
+        image.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -60).isActive = true
         
         
         
