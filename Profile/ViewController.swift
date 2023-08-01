b5298e3556cc757ec403fe250853e7aa66088101
Yaroslav <neptun2001yarik@gmail.com>
2023-08-01 17:03:27 +0300

    with All text

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
         let image: UIView = {
             let view = UIView()
             view.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
-            let image0 = UIImage(named: "Image.png")?.cgImage
+            let image0 = UIImage(named: "ph.png")?.cgImage
             let layer0 = CALayer()
             layer0.contents = image0
             layer0.bounds = view.bounds
@@ -44,35 +44,56 @@ class ViewController: UIViewController {
             label.numberOfLines = 2
             label.text = "Савин Ярослав Николаевич"
             label.font = UIFont.systemFont(ofSize: 30)
-            var paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.lineHeightMultiple = 1.05
             label.textAlignment = .center
-
-//            label.frame = CGRect(x: 0, y: 0, width: 149, height: 64)
-
+            return label
+        }()
+        let job: UILabel = {
+           let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 1
+            label.text = "Junior iOS-разработчик, опыт 1 год"
+            label.textColor = UIColor(red: 0.588, green: 0.584, blue: 0.608, alpha: 1)
+            label.textAlignment = .center
+            return label
+        }()
+        let city: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 1
+            label.text = "Москва"
+            label.textAlignment = .center
+            label.textColor = UIColor(red: 0.588, green: 0.584, blue: 0.608, alpha: 1)
             return label
         }()
 
         view.addSubview(headText)
         view.addSubview(image)
         view.addSubview(name)
+        view.addSubview(job)
+        view.addSubview(city)
 
         
+        setUpConstraint()
         
-        headText.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        headText.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        headText.topAnchor.constraint(equalTo: view.topAnchor, constant: 62).isActive = true
-        image.topAnchor.constraint(equalTo: headText.bottomAnchor, constant: 50).isActive = true
-        image.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -60).isActive = true
-        name.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 150).isActive = true
-        name.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        name.widthAnchor.constraint(equalToConstant: 350).isActive = true
 
         
-        
+        func setUpConstraint() {
+            headText.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            headText.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            headText.topAnchor.constraint(equalTo: view.topAnchor, constant: 62).isActive = true
+            image.topAnchor.constraint(equalTo: headText.bottomAnchor, constant: 50).isActive = true
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -60).isActive = true
+            name.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 150).isActive = true
+            name.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            name.widthAnchor.constraint(equalToConstant: 350).isActive = true
+            job.topAnchor.constraint(equalTo: name.bottomAnchor).isActive = true
+            job.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            city.topAnchor.constraint(equalTo: job.bottomAnchor, constant: 20).isActive = true
+            city.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        }
         
     }
 
-
+    
 }
 
